a52981ab46c51cb3b876707b0cda9b77e8158c59
shubhamjha2510 <shubhamjha2510@gmail.com>
2025-06-28 18:27:36 +0530

    Update project with latest changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,8 +6,8 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "BSoDium API",
-  description: "The API for the landing page at bsodium.fr",
+  title: "Shubham API",
+  description: "The API for the landing page at api.theshubhamjha.com",
 };
 
 export default function RootLayout({
